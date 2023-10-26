3244aafad25daa57ad1b54bf71adde35133b19f5
abubakar460 <mdsimul5060@gmail.com>
2023-10-26 09:43:34 -0700

    sd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-import Cursor from '@/Components/Cursor'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-slate-800">
-      <Cursor />
+     
      
         {children}
         </body>
