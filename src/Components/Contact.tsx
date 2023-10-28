a53b85f035e4ec66250a93b0734c0809e7a40efe
abubakar460 <mdsimul5060@gmail.com>
2023-10-28 12:19:24 -0700

    tittle

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -13,14 +13,16 @@ import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div className='grid grid-cols-12 gap-6 px-5 my-14 lg:px-48 sm:px-20 md:px-32'>
+    <div 
+
+    className='grid grid-cols-12 gap-6 px-5 my-14 lg:px-48 sm:px-20 md:px-32'>
     <div className='col-span-12 bg-slate-900 lg:col-span-3 p-4 text-center  rounded-2xl'>
       <Sidebar /> 
     </div>
     <div className='col-span-12 bg-slate-900 lg:col-span-9  rounded-2xl'>
     <Rightbar />
     <div  className='    '>
-        <motion.div 
+        <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{
